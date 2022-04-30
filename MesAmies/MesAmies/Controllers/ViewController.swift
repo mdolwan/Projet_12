0442e24dc5279d4a1d1b50d633bb82c468e343fb
OLWAN Mohammad <modowan75@gmail.com>
2022-04-30 14:26:43 +0200

    Traite the root viewcontroller

--- MesAmies/MesAmies/Controllers/ViewController.swift
+++ MesAmies/MesAmies/Controllers/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+            
+            // This is to get the SceneDelegate object from your view controller
+            // then call the change root view controller function to change to main tab bar
+            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
 
+    }
+    
 }
 
